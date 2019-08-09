bdd446512dc6f00e991ad6dd284c1463459c2f9d
Matt. Toczek <toczek.matt@gmail.com>
2019-08-09 07:09:14 +0100

    adding detail after links, based on classes assigned in jQuery

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-// const $odd = $('a:odd');
+const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
 // $pdfs.hide();
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
+
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
