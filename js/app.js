5c598100f3f550ee43c11c884393e804f7f39577
Matt. Toczek <toczek.matt@gmail.com>
2019-08-14 09:40:17 +0100

    Adding checkbox restriction to pdf download

--- js/app.js
+++ js/app.js
@@ -3,8 +3,26 @@ const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
 // $pdfs.hide();
 
+
+
+//open new tab for secure links
 $secureLinks.attr('target', '_blank');
-$pdfs.attr('download', true);
+
+//auto download pdfs or open in new tab
+$pdfs.attr('download', true)
+      .attr('target', '_blank');
 
 $secureLinks.addClass('secure');
 $pdfs.addClass('pdf');
+
+$pdfs.on('click', function(event){
+  //check if box is checked
+  //if zero checkboxes = checked
+  if($(':checked').length === 0){
+      //prevent download
+    event.preventDefault();
+    //alert user
+    alert('Please check the box to allow pdf downloads')
+  }
+  //else, allow download
+});
